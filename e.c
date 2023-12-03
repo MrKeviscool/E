c6d4159ff12ee80838330597dfa13ae3f200d9ce
MrKeviscool <77475385+MrKeviscool@users.noreply.github.com>
2023-12-03 19:07:07 +1030

    E
    E

---
+++ e.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <time.h>
+
+#define e (
+#define ee )
+
+#define eee {
+#define eeee }
+
+#define eeeee void
+#define eeeeee main
+#define eeeeeee printf
+#define eeeeeeee while
+#define eeeeeeeee int
+#define eeeeeeeeee prevtime
+#define eeeeeeeeeee clock
+#define eeeeeeeeeeee for
+#define eeeeeeeeeeeee i
+#define eeeeeeeeeeeeee =
+#define eeeeeeeeeeeeeee ++
+#define eeeeeeeeeeeeeeee <
+#define eeeeeeeeeeeeeeeee "hello world!"
+#define eeeeeeeeeeeeeeeeee char
+#define eeeeeeeeeeeeeeeeeee hworld
+
+#define eeeeeeeeeeeeeeeeeeee [
+#define eeeeeeeeeeeeeeeeeeeee ]
+#define eeeeeeeeeeeeeeeeeeeeee sizeof
+#define eeeeeeeeeeeeeeeeeeeeeee 1000000
+#define eeeeeeeeeeeeeeeeeeeeeeee "%d"
+#define eeeeeeeeeeeeeeeeeeeeeeeee "%c"
+#define eeeeeeeeeeeeeeeeeeeeeeeeee ,
+#define eeeeeeeeeeeeeeeeeeeeeeeeeee "\n"
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeee "look at this cool slow text brug"
+
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeee 200 //<-- delay
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeee +
+
+eeeeeeeee eeeeeeeeee eeeeeeeeeeeeee 0;
+eeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeee;
+
+eeeee eeeeee e ee eee
+    eeeeeee e eeeeeeeeeeeeeeeee ee;
+    eeeeeee e eeeeeeeeeeeeeeeeeeeeeeeeeee ee;
+    eeeeeeeeeeee e eeeeeeeee eeeeeeeeeeeee eeeeeeeeeeeeee 0; eeeeeeeeeeeee eeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeee e eeeeeeeeeeeeeeeeeee ee; eeeeeeeeeeeee eeeeeeeeeeeeeee ee eee
+        eeeeeee e eeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeee eeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeee  ee;
+
+        eeeeeeee e eeeeeeeeeee e ee eeeeeeeeeeeeeeee eeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeeee ee eee eeee
+        eeeeeeeeee eeeeeeeeeeeeee eeeeeeeeeee e ee;
+
+    eeee
+    eeeeeee e eeeeeeeeeeeeeeeeeeeeeeeeeee ee;
+
+eeee
